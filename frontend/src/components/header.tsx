8e124fd8962fa858946a3ee20ec27aaed5459aab
Kidus Abebe <kidsebk@gmail.com>
2024-12-31 17:44:20 +0300

    add: header and footer

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,13 +1,14 @@
-import { ThemeSwitcher } from "./theme-switcher"
+import { ThemeSwitcher } from "./theme-switcher.tsx"
+import React from "react"
 
 export default function Header() {
-  return (
-    <header className="bg-gruvbox-light1 dark:bg-gruvbox-dark1 text-gruvbox-dark0 dark:text-gruvbox-light0 p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Song Manager</h1>
-        <ThemeSwitcher />
-      </div>
-    </header>
-  )
+	return (
+		<header className="bg-gruvbox-dark1 text-gruvbox-light0 p-4 shadow-md">
+			<div className="container mx-auto flex justify-between items-center">
+				<h1 className="text-2xl font-bold">Song Manager</h1>
+				<ThemeSwitcher />
+			</div>
+		</header>
+	)
 }
 
