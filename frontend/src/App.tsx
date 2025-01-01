4ec1d82be429220f1709f0539c600d411421ec5d
Kidus Abebe <kidsebk@gmail.com>
2025-01-01 21:01:49 +0300

    add: root layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import RootLayout from './layout.tsx';
+import Home from './page.tsx';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<RootLayout>
+			<div className="min-h-screen flex flex-col bg-gruvbox-light0 dark:bg-gruvbox-dark0">
+				<Home />
+			</div>
+		</RootLayout>
 	);
 }
 
